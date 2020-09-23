a3ddfc1d4939361ee57fb9e84febf810f2e3ad74
rgirodon <remy.girodon@gmail.com>
2020-09-23 17:13:58 +0200

    Adapt samples to Fise2 S2

--- activity2_ES6Module.mjs
+++ activity2_ES6Module.mjs
@@ -1,4 +1,4 @@
-import {add, minus, multiply, divide} from "./exportEs6Module.mjs";
+import {add, minus, multiply, divide} from "./arithmetics_Es6Module.mjs";
 
 console.log(add(1, 2));
 
@@ -8,9 +8,10 @@ console.log(multiply(11, 12));
 
 console.log(divide(10, 2));
 
+/*
 import Player from "./exportDefaultEs6Module.mjs";
 
 let player = new Player('Leo', 'Messi');
 
 console.log(player.getFullName());
-
+*/
